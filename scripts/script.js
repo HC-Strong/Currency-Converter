8f44de5af4db2f3e180c68523f1f4cc8c966db3a
HC-Strong <stronghannahc@gmail.com>
2021-05-20 11:44:51 -0700

    Initial Commit & Basic fetch
    Initial Commit & Basic fetch of USD to CAD conversion, results shown on page via jquery

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+console.log("started");
+const apiKey = "4438c1a06663d4c28f979c38";
+const resultPar = jQuery("#result");
+let result = fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`)
+  .then(response => response.json())
+  .then(json => displayResult(json));
+
+function displayResult(json) {
+  jQuery(resultPar).text(json.conversion_rates.CAD);
+  console.log("done");
+}
