70459a4ee60e9ebbdef0286c2afb718db2c97327
HC-Strong <stronghannahc@gmail.com>
2021-05-21 14:38:32 -0700

    Form submission now works
    User can enter an amount, currency code to convert from and code to convert to and the result is displayed. No error checking

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
-console.log("started");
 const apiKey = "4438c1a06663d4c28f979c38";
+//https://v6.exchangerate-api.com/v6/4438c1a06663d4c28f979c38/latest/USD
 const resultPar = jQuery("#result");
 
-let result = fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`)
-  .then(response => response.json())
-  .then(json => displayResult(json));
+const baseUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/`;
 
-function displayResult(json) {
-  jQuery(resultPar).text(json.conversion_rates.CAD);
-  console.log("done");
+function displayResult(json, amount) {
+  let resultString = `${amount} ${json.base_code} is equal to ${json.conversion_result.toFixed(2)} ${json.target_code}`;
+  jQuery(resultPar).addClass("full").text(resultString);
+  console.log("result displayed");
 }
+
+document.querySelector("#converter-form")
+.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const fromCurrency = event.target.from.value;
+  const toCurrency = event.target.to.value;
+  const amount = event.target.amount.value;
+  let requestUrl = baseUrl + `/${fromCurrency}/${toCurrency}/${amount}/`;
+  let result = fetch(requestUrl)
+    .then(response => response.json())
+    .then(json => displayResult(json, amount));
+
+});
