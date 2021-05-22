18897e96b7c7bcca84e73882981ef2dcab6c9816
HC-Strong <stronghannahc@gmail.com>
2021-05-21 18:45:12 -0700

    Formatting Form & Including Currency Name
    Reformatted form layout
    Displayed currency name under dropdown

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,7 +1,5 @@
 // Generates Currency Options for dropdown form selection
 // Uses supportedCurrencies constant in currencies.js as source of list
-
-
 let datalist = jQuery("#currencies");
 
 $.each(supportedCurrencies, function (i, item) {
@@ -10,5 +8,14 @@ $.each(supportedCurrencies, function (i, item) {
         text : item.name
     }));
 });
-
 console.log("Currency Dropdowns Created");
+
+//Add event listener to dropdowns to update the visible output name when the dropdown changes
+jQuery(".currency-input").on('change',function(){
+
+  let val = event.target.value;
+  let text = supportedCurrencies[val].name;
+  let name = event.target.name;
+  console.log($(this).val() + ", " + supportedCurrencies[$(this).val()].name);
+  jQuery('output[for="'+name+'"]').text(text);
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,18 @@
+// Set up base URL for API requests
 const apiKey = "4438c1a06663d4c28f979c38";
-//https://v6.exchangerate-api.com/v6/4438c1a06663d4c28f979c38/latest/USD
 const resultPar = jQuery("#result");
-
 const baseUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/`;
 
+
+// Display results returned from form submission
 function displayResult(json, amount) {
   let resultString = `${amount} ${json.base_code} is equal to ${json.conversion_result.toFixed(2)} ${json.target_code}`;
   jQuery(resultPar).addClass("full").text(resultString);
   console.log("result displayed");
 }
 
+
+// Add event listener to form to send API request and handle error messages with alert
 document.querySelector("#converter-form")
 .addEventListener("submit", (event) => {
   event.preventDefault();
